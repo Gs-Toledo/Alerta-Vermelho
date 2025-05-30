e9407953c75f25c024352f1e7db6c5bfb8842f1c
Isaac Ferreira <zaclummys@gmail.com>
2025-05-30 16:54:40 +0000

    Add more e2e tests to title screen

--- packages/react/tests/title-screen.spec.ts
+++ packages/react/tests/title-screen.spec.ts
@@ -6,7 +6,27 @@ test('has title', async ({ page }) => {
     await expect(page).toHaveTitle("Alerta Vermelho");
 });
 
-test('can start the game', async ({ page }) => {
+test('has game title', async ({ page }) => {
+    await page.goto('/');
+
+    const gameTitle = page.getByRole('heading', {
+        name: 'Alerta Vermelho'
+    });
+
+    await expect(gameTitle).toBeVisible();
+});
+
+test('has start button', async ({ page }) => {
+    await page.goto('/');
+
+    const startButton = page.getByRole('button', {
+        name: 'Start'
+    });
+
+    await expect(startButton).toBeVisible();
+});
+
+test('can redirect to game page by clicking in the start button', async ({ page }) => {
     await page.goto('/');
 
     const startButton = page.getByRole('button', {
