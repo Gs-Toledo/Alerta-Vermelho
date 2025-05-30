20eb5f7f1ea0e51b139b4e7c3013f42a94da5d82
Isaac Ferreira <zaclummys@gmail.com>
2025-05-30 17:04:31 +0000

    Create Game component

---
+++ packages/react/src/Game.tsx
@@ -0,0 +1,13 @@
+export default function Game () {
+    return (
+        <div>
+            <span role="heading">
+                Alerta Vermelho
+            </span>
+
+            <button role="button">
+                Start
+            </button>
+        </div>
+    )
+};
