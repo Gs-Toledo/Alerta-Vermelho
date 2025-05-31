dc71deebc20e026274c7430a1cc1742e89d7e7b8
Isaac Ferreira <zaclummys@gmail.com>
2025-05-31 19:45:47 +0000

    Simplifica os testes de fim a fim

--- packages/react/tests/title-screen.spec.ts
+++ packages/react/tests/title-screen.spec.ts
@@ -1,53 +1,33 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+async function goToStartScreen (page) {
     await page.goto('/');
+}
 
-    await expect(page).toHaveTitle("Alerta Vermelho");
-});
-
-test('has heading', async ({ page }) => {
-    await page.goto('/');
-
-    const gameTitle = page.getByRole('heading', {
-        name: 'Alerta Vermelho'
-    });
-
-    await expect(gameTitle).toBeVisible();
-});
-
-test('has start button', async ({ page }) => {
-    await page.goto('/');
-
-    const startButton = page.getByRole('button', {
-        name: 'Iniciar'
-    });
-
-    await expect(startButton).toBeVisible();
-});
-
-test('has help button', async ({ page }) => {
-    await page.goto('/');
-
-    const helpButton = page.getByRole('button', {
-        name: 'Ajuda'
-    });
-
-    await expect(helpButton).toBeVisible();
-});
-
-test('has exit button', async ({ page }) => {
-    await page.goto('/');
+async function startGame (page) {
+    await goToStartScreen(page);
 
     const startButton = page.getByRole('button', {
         name: 'Iniciar'
     });
 
     await startButton.click();
+}
+
+async function exitGame (page) {
+    await startGame(page);
 
     const exitButton = page.getByRole('button', {
         name: 'Sair'
     });
 
-    await expect(exitButton).toBeVisible();
+    await exitButton.click();
+}
+
+test('can start the game', async ({ page }) => {
+    await startGame(page);
+});
+
+test('can exit the game', async ({ page }) => {
+    await exitGame(page);
 });
